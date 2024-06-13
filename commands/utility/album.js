2e82b9365a07aabc36182541ac42279374a5ef5c
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-13 13:02:22 +0200

    Improved album command with embeds
    Expanded the `album` bot command in discord.js to reply with a detailed, rich embed. The update includes setting the color scheme, title, description, thumbnail, multiple fields, an image, timestamp, and footer alongside the command reply.

--- commands/utility/album.js
+++ commands/utility/album.js
@@ -1,10 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+
+const embed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Some title')
+    .setDescription('')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('album')
         .setDescription('Provides information about the album.'),
     async execute(interaction) {
-        await interaction.reply(`Reply to "album" works ✅`);
+        await interaction.reply({embeds: [embed]});
     },
 };
