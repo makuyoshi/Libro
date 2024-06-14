5eb5894a26eb745948cd129bc35bba2d97d68007
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-14 13:30:31 +0200

    Update book search functionality in searchAuthor.js
    The searchAuthor.js file was updated to improve the utility of the book search function. The query function was edited to search for given book authors instead of a pre-defined author. The response capacity was also increased to report seven instead of five books.

--- commands/utility/searchAuthor.js
+++ commands/utility/searchAuthor.js
@@ -3,8 +3,8 @@ const { googleBooksApiKey } = require('../../config.json');
 
 const authorInfoTemplate = {
     color: 0x0099ff,
-    title: 'Book Titles',
-    url: 'https://discord.js.org',
+    title: '',
+    url: '',
     description: 'Found books matching query.',
     thumbnail: {
         url: 'https://i.imgur.com/AfFp7pu.png',
@@ -17,22 +17,24 @@ module.exports = {
         .setName('sa')
         .setDescription('/sa <query> | Search for books.'),
     async execute(interaction) {
-        const query = "" /*interaction.options.getString('query');*/
+        const query = "Brandon%20Sanderson" /*interaction.options.getString('query');*/
         const authorInfo = {...authorInfoTemplate };
 
         try {
-            const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=Brandon%20Sanderson&key=${googleBooksApiKey}`);
+            const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}&key=${googleBooksApiKey}`);
             const data = await response.json();
 
             console.log(data)
 
+            authorInfo.title = query;
+
             const books = data.items.map(item => ({
                 name: `${item.volumeInfo.title? item.volumeInfo.title : 'Unknown Title'}`,
-                value: item.volumeInfo.publishedDate.join(', ') || 'No authors found',
+                value: item.volumeInfo.publishedDate || 'No date found',
                 inline: true
             }));
 
-            authorInfo.fields = books.slice(0, 5);
+            authorInfo.fields = books.slice(0, 7);
 
             await interaction.reply({ embeds: [authorInfo] });
         } catch (error) {
