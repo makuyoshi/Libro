b873bb69267463bd14234c290f25071a7e849fd5
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-17 16:09:57 +0200

    Enhance '/author' command functionality
    The 'author' command was improved by adding an embedded message feature and making the command interactive. Now, it fetches author's data from Open Library API, displays the author's name, biography, and picture. If no author is found, it handles the situation gracefully.

--- commands/utility/author.js
+++ commands/utility/author.js
@@ -1,11 +1,40 @@
-// Fetch and display information about an author, including their most popular works, biography, and photo.
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('author')
-        .setDescription('/author <author_name> | Get information about an author.'),
+        .setDescription('/author <author_name> | Get information about an author.')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true)),
     async execute(interaction) {
-        await interaction.reply("Reply under construction");
+        const authorName = interaction.options.getString('input');
+        try {
+            const url = `https://openlibrary.org/search/authors.json?q=${encodeURIComponent(authorName)}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            const authors = data.docs;
+
+            if (authors.length > 0) {
+                const author = authors[0];
+
+                const embed = new EmbedBuilder()
+                    .setColor('#0099ff')
+                    .setTitle(`${author.name}`)
+                    .setURL(`https://openlibrary.org${author.url}`)
+                    .setDescription(`Biography: ${author.bio}`)
+                    .setImage(author.author_image);
+
+                await interaction.reply({ embeds: [embed] });
+            } else {
+                await interaction.reply(`Author "${authorName}" not found.`);
+            }
+        } catch (error) {
+            console.error(error);
+            await interaction.reply('An error occurred while fetching author information.');
+        }
     },
+
 };
